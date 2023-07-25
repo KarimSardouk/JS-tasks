54fbdb2d640eb6b151552268ab23a6e8b4b3e2ef
Karim Sardouk <karimsardouk727@gmail.com>
2023-07-25 16:09:09 +0300

    Done with task 3

---
+++ task_3/script.js
@@ -0,0 +1,13 @@
+const num1 = 20;
+var num2 = 30;
+var canPass = true;
+if (canPass)
+    if (num1 > num2) {
+        //  block of code to be executed if the condition is true
+        console.log("Hello");
+    }
+    else { console.log("Bye"); }
+
+else {
+    console.log("Try again!");
+}
