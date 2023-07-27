52c8edc9c12e5c2614cfe526c30672a88e7d4853
Karim Sardouk <karimsardouk727@gmail.com>
2023-07-27 12:15:20 +0300

    Done task 8

--- Task_7/script.js
+++ Task_7/script.js
@@ -22,3 +22,5 @@ for (let i = 0; i < numbers.length; i++) {
     console.log(number);
   }
 }
+
+
---
+++ Task_8/script.js
@@ -0,0 +1,8 @@
+//random number with let
+let randomNumber2 = Math.floor(Math.random() * 10) + 1;
+//while loop
+while (randomNumber2 <= 8) {
+    randomNumber2 = Math.floor(Math.random() * 10) + 1;
+    //output of random
+    console.log(randomNumber2);
+}
